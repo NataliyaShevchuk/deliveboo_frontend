4fcda5d604f14337fe9b7ea24e7a80d0d39f6057
marcopisani002 <pisanimarco34@gmail.com>
2023-03-16 12:35:24 +0100

    add show

--- src/pages/restaurants/Show.vue
+++ src/pages/restaurants/Show.vue
@@ -6,14 +6,34 @@ export default{
     data(){
         return{
             backendUrl : 'http://localhost:8000',
+           restaurant: {},
         }
+
+        
+    },
+    methods: {
+    fetchRestaurants() {
+      axios.get(this.backendUrl + "/api/restaurants/" + this.$route.params.id)
+        .then((resp) => {
+          this.restaurant = resp.data;
+
+          console.log(this.restaurant);
+        });
     }
+  },
 }
 </script>
 
-
 <template>
-    <div>
-        <h1>Restaurant Show</h1>
+    <div class="container text-center text-warning mt-4">
+      <h1>Pagina dettagli progetto {{ $route.params.id }}</h1>
+      <h2>{{ restaurant.name }}</h2>
+  
+      <div >
+        <img :src="restaurant.cover_img" class="img-fluid" />
+      </div>
+  
+      <p class="lead">{{ restaurant.phone_number }}</p>
     </div>
-</template>
+  </template>
+  
