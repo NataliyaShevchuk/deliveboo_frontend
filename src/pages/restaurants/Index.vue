6738bd1d0ca5a3379d0341aec9c78bdbf9b4474d
NataliyaShevchuk <nata.shevchuk@icloud.com>
2023-03-16 15:37:38 +0100

    temporery changes

--- src/pages/restaurants/Index.vue
+++ src/pages/restaurants/Index.vue
@@ -27,27 +27,47 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="row justify-content-evenly m-4">
-      <div class="col-6" v-for="restaurant in restaurants" :key="restaurants.id">
-        <div class="card mb-3" style="max-width: 540px;">
-          <div class="row g-0">
-            <div class="col-md-4" v-if="restaurant.cover_img">
-              <img :src="this.backendUrl + '/storage/' + restaurant.cover_img" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">{{ restaurant.name }}</h5>
-                <p class="card-text">{{ restaurant.description }}</p>
-                <p class="card-text">{{ restaurant.phone_number }}</p>
-                <p class="card-text">{{ restaurant.address }}</p>
-                <p class="card-text"><small class="text-muted">{{ restaurant.vat }}</small></p>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-3 m-auto mt-4">
+        <div class="list-group">
+          <h4>Di che cosa hai voglia oggi?</h4>
+          <div class="list-group-item">
+            <label>
+              <input type="checkbox" class="form-check-input">
+              Pizze ria
+            </label>
+          </div>
+          <button type="button" class="btn btn-ligth mt-4">Cerca</button>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-md-9 ">
+      <div class="row justify-content-evenly m-4">
+        <div class="col-sm-4 col-md-4 col-lg-4" v-for="restaurant in restaurants" :key="restaurants.id">
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4" v-if="restaurant.cover_img">
+                <img :src="this.backendUrl + '/storage/' + restaurant.cover_img" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{ restaurant.name }}</h5>
+                  <p class="card-text">{{ restaurant.description }}</p>
+                  <p class="card-text">{{ restaurant.phone_number }}</p>
+                  <p class="card-text">{{ restaurant.address }}</p>
+                  <p class="card-text"><small class="text-muted">{{ restaurant.vat }}</small></p>
+                  <h5 class="card-title">{{ restaurant.type_id }}</h5>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
